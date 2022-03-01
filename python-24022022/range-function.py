19e8363f4f388450f5f98546f9fe008cb1f6fa27
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2022-03-01 10:59:09 +0530

    Update range-function.py

--- python-24022022/range-function.py
+++ python-24022022/range-function.py
@@ -1,9 +1,34 @@
+Syntax od range():
+    range(stop)
+    range(start, stop, [, step])
+    
+1)  
+print (list (range(10)))
+
+
+2)
 from __future__ import print_function
 for i in range(1, 5):
     for j in range(i):
          print(i, end=' ')
     print()
 
+
+3)    
+#printing first 5
+#whole number
+
+for i in range(5):
+    print(i, end=" ")
+print()
+
+# printing first 15
+#whole number
+
+for i in range(15):
+    print(i, end=" ")
+    
+4)
     
     
  
