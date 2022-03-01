c4afd0af4ef94f7e87d7d528c75205c3e8c146ad
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2022-03-01 11:19:52 +0530

    Update range-function.py

--- python-24022022/range-function.py
+++ python-24022022/range-function.py
@@ -5,6 +5,10 @@ Syntax od range():
 1)  
 print (list (range(10)))
 
+print (list (range(20)))
+
+print (list (range(15)))
+
 
 2)
 from __future__ import print_function
@@ -29,6 +33,45 @@ for i in range(15):
     print(i, end=" ")
     
 4)
-    
+# Python program to
+# print all number
+# divisible by 3 and 5
+
+# using range to print number
+# divisible by 3
+for i in range(0, 30, 3):
+    print(i, end=" ")
+print()
+
+-----------------
+
+
+# incremented by 2
+for i in range(2, 20, 2):
+    print(i, end=" ")
+print()
+
+# incremented by 4
+for i in range(0, 25, 4):
+    print(i, end=" ")
+print()
+
+# incremented by 3
+for i in range(15, 30, 3):
+    print(i, end=" ")
+--------------------------
+
+Concatenation of two range() functions
+
+# Python program to concatenate
+# the result of two range functions
+from itertools import chain
+
+# Using chain method
+print("Concatenating the result")
+res = chain(range(5), range(10, 20, 2))
+
+for i in res:
+    print(i, end=" ")
     
  
