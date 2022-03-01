68972363f74610cd229d4115c07ca91e0ad11304
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2022-03-01 12:14:35 +0530

    Update control-statemetns.py

--- python-25022022/control-statemetns.py
+++ python-25022022/control-statemetns.py
@@ -4,10 +4,57 @@ while (count < 3):
     count = count+1
     print("python programs")
     
- -> 
+ 
 a = 33
 b = 200
 if b > a:
   print("b is greater than a")
+
+
+a = 96
+b = 96
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+---------------
+->Else
+ The else keyword catches anything which isn't caught by the preceding conditions.
+    
+In this example a is greater than b, so the first condition is not true, also the elif condition is not true, 
+so we go to the else condition and print to screen that "a is greater than b".
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+-----------------------------
+
+Nested If
+You can have if statements inside if statements, this is called nested if statements.
+
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
     
     
