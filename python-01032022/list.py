d79c45f7c2be46360119766ca666b2d99c72f6e7
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2022-03-01 13:59:07 +0530

    Update list.py

--- python-01032022/list.py
+++ python-01032022/list.py
@@ -1,2 +1,36 @@
-list
-fdg
+List methods in Python
+___________________________
+
+Types of List Method
+----------------------------
+Python List index()
+Python List append()
+Python List extend()
+Python List insert()
+Python List remove()
+Python List count()
+Python List pop()
+Python List reverse()
+Python List sort()
+Python List copy()
+Python List clear()
+
+
+(1) List index() 
+    
+index(): Returns the index of first occurrence. Start and End index are not necessary parameters.
+-> The index() method returns the index of the specified element in the list.
+Syntax:
+List.index(element[,start[,end]])
+
+
+Programs:
+_________
+1)
+language = ['c', 'java', 'python', 'groovy']
+
+# get the index of 'python'
+index = language.index('python')
+
+
+print(index)
