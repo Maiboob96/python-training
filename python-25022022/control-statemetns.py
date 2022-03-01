a4c82379ee616749b74a39b4c618f8f57aa16f96
Mehboob Choudri <82254400+Maiboob96@users.noreply.github.com>
2022-03-01 11:59:41 +0530

    Update and rename control.py to control-statemetns.py

---
+++ python-25022022/control-statemetns.py
@@ -0,0 +1,13 @@
+-> # prints python programs 3 Times
+count = 0
+while (count < 3):
+    count = count+1
+    print("python programs")
+    
+ -> 
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+    
+    
